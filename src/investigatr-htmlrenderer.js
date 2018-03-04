01b88fe27db193a598997891efc92b9391fe7d7d
Rabrennie <Rabrennie@users.noreply.github.com>
2018-03-04 02:38:27 +0000

    Update styling of html renderer

--- src/investigatr-htmlrenderer.js
+++ src/investigatr-htmlrenderer.js
@@ -5,7 +5,8 @@ function InvestigatrHtmlRenderer(options) {
 
     // TODO: refactor
     this.render = function(time, results) {
-        
+        this.options.containerEl.style.fontfamily = "Arial";
+        console.log(this.options.containerEl.style);
         const timeEl = document.createElement('div');
         const totalTests = document.createElement('div');
         timeEl.innerHTML = `elapsed: ${time} secs`;
@@ -20,25 +21,24 @@ function InvestigatrHtmlRenderer(options) {
             if (results.hasOwnProperty(groupName)) {
                 const group = results[groupName];
                 const resultEl = document.createElement('div');
-                resultEl.innerHTML = `<h3>${groupName}</h3><ul></ul>`;
+                resultEl.innerHTML = `<h3 style="font-weight: 400">${groupName}</h3><ul style="list-style:none"></ul>`;
                 for (const testName in group) {
                     if (group.hasOwnProperty(testName)) {
                         const testResult = group[testName];
                         const testEl = document.createElement('li');
-                        testEl.style.cursor = 'pointer';
                         testCount += 1;
                         if(testResult.pass) {
                             passCount += 1
                         }
-                        testEl.innerHTML = `<div> ${testResult.pass ? 'pass': 'fail'}  - ${testName}</div>`;
+                        testEl.innerHTML = `<div style="cursor: pointer; color: ${testResult.pass ? '#9c9c9c':'#a90303;'}"> ${testResult.pass ? '&#10003;': '&#10005;'} ${testName}</div>`;
                         if(testResult.assertion != null) {
-                            testEl.innerHTML += `<pre style="color: red;">${testResult.assertion}</pre>`;
+                            testEl.innerHTML += `<pre style="margin-left: 30px;color: #ff6565;">${testResult.assertion}</pre>`;
                         }
                         if(testResult.error != null) {
-                            testEl.innerHTML += `<pre style="color: red;">${testResult.error}</pre>`;
+                            testEl.innerHTML += `<pre style="margin-left: 30px;color: #ff6565;">${testResult.error}</pre>`;
                         }
                         if(testResult.source != null) {
-                            testEl.innerHTML += `<pre class="test-source" style="display: none">${testResult.source.split('\n        ').join('\n')}</pre>`;
+                            testEl.innerHTML += `<pre class="test-source" style="margin-left: 30px; display: none">${testResult.source.split('\n        ').join('\n')}</pre>`;
                         
                         }
                         testEl.childNodes[0].addEventListener('click', function() {
