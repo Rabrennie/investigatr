9e56bcc2aff59181e0b7d1cf0e1a0deb4e3fc5d2
Rabrennie <Rabrennie@users.noreply.github.com>
2018-03-04 02:04:37 +0000

    add tests for assertEqual and assertNotEqual

--- tests/spec.js
+++ tests/spec.js
@@ -22,7 +22,7 @@ testGroup('Investigatr', {
             return assertEquals(data.investigatr.options.renderer, "test");
         },
     }
-})
+});
 
 testGroup('.assertEqual()', {
     data: {},
@@ -35,6 +35,31 @@ testGroup('.assertEqual()', {
         "result is false when 1 == 2 ": function(data) {
             let result = assertEquals(1,2).result;
             return assertEquals(result, false);
+        },
+
+        "message is 'expected(2) is not equal to actual(1)' when 1 == 2 ": function(data) {
+            let message = assertEquals(1,2).message;
+            return assertEquals(message, 'expected(2) is not equal to actual(1)');
+        }
+    }
+});
+
+testGroup('.assertNotEqual()', {
+    data: {},
+    tests: {
+        "result is false when 1 == 1 ": function(data) {
+            let result = assertNotEquals(1,1).result;
+            return assertEquals(result, false);
+        },
+
+        "result is true when 1 == 2 ": function(data) {
+            let result = assertNotEquals(1,2).result;
+            return assertEquals(result, true);
+        },
+
+        "message is 'expected(1) is equal to actual(1)' when 1 == 1 ": function(data) {
+            let message = assertNotEquals(1,1).message;
+            return assertEquals(message, 'expected(1) is equal to actual(1)');
         }
     }
 })
