386b0eb802f9c711975ff8695c5915f54f6d9e2a
Rabrennie <Rabrennie@users.noreply.github.com>
2018-03-04 01:39:34 +0000

    Add pass percentage to html renderer

--- src/investigatr-htmlrenderer.js
+++ src/investigatr-htmlrenderer.js
@@ -6,26 +6,38 @@ function InvestigatrHtmlRenderer(options) {
     this.render = function(time, results) {
         
         const timeEl = document.createElement('div');
+        const totalTests = document.createElement('div');
         timeEl.innerHTML = `elapsed: ${time} secs`;
 
         this.options.containerEl.appendChild(timeEl);
+        this.options.containerEl.appendChild(totalTests);
+
+        let testCount = 0;
+        let passCount = 0;
 
         for (const result in results) {
             if (results.hasOwnProperty(result)) {
                 const element = results[result];
-                const resultEl = document.createElement('ul');
+                const resultEl = document.createElement('div');
+                resultEl.innerHTML = `<h3>${result}</h3><ul></ul>`;
                 for (const test in element) {
                     if (element.hasOwnProperty(test)) {
                         const testResult = element[test];
                         const testEl = document.createElement('li');
+                        testCount += 1;
+                        if(testResult.pass) {
+                            passCount += 1
+                        }
                         testEl.innerHTML = testResult.pass ? 'pass': 'fail';
                         testEl.innerHTML += ` - ${test} <pre>${testResult.assertion}</pre><pre>${testResult.error}</pre><pre>${testResult.source.split('\n        ').join('\n')}</pre>`
-                        resultEl.appendChild(testEl);
+                        resultEl.querySelector('ul').appendChild(testEl);
                     }
                 }
                 this.options.containerEl.appendChild(resultEl);
             }
         }
+
+        totalTests.innerHTML = `${(passCount / testCount * 100).toFixed(0)}%`
 
     }
 }
